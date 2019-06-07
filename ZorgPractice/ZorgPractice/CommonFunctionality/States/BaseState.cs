d15e8451ceca00d9ed77bda91fec20c061a8b5f1
Mambii <dwoodhouse1@hotmail.co.uk>
2019-06-07 16:33:17 +0100

    Interface State Thingies

---
+++ ZorgPractice/ZorgPractice/CommonFunctionality/States/BaseState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZorgPractice.CommonFunctionality.States
+{
+	public class BaseState : Istate
+	{
+
+		#region Constructors
+		public BaseState()
+		{
+
+		}
+		#endregion
+
+		public void Finish()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Initalize()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PerformTask()
+		{
+			throw new NotImplementedException();
+		}
+
+		public event Action<Type> RequestStateChange;
+
+		public void ChangeState<Type>()
+		{
+			RequestStateChange.Invoke(typeof(Type));
+		}
+	}
+}
---
+++ ZorgPractice/ZorgPractice/CommonFunctionality/States/Istate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZorgPractice.CommonFunctionality.States
+{
+	public interface Istate
+	{
+		void Initalize();
+		void PerformTask();
+		event Action<Type> RequestStateChange;
+		void Finish();
+	}
+}
